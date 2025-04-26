b64b1563d3c412d7cf7bc02baaf8c093152bcead
Glib-xx <sevensky.ga@gmail.com>
2025-04-26 19:24:45 +0300

    update js/menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,42 +1,13 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
+  const openMenuBtn = document.querySelector('.mobile-menu-open-btn');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+  openMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('is-open');
+  });
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-
-    if (anchors.length === 0) return;
-
-    if (!isMenuOpen) {
-      anchors.forEach(anchor => {
-        anchor.addEventListener('click', toggleMenu);
-      });
-      return;
-    }
-
-    anchors.forEach(anchor => {
-      anchor.removeEventListener('click', toggleMenu);
-    });
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Вказати брейкпоінт після якого повинна зачинятися
-  window.matchMedia('(max-width: 1280px)').addEventListener('change', e => {
-    if (!e.matches) return;
+  closeMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
